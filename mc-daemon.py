8e715dadfb56c37d42435e68480a2503aa1af6e5
suuniqo <suniqo.me@gmail.com>
2025-07-11 12:58:43 +0100

    fixed setup_hook

--- mc-daemon.py
+++ mc-daemon.py
@@ -176,7 +176,7 @@ class ServerManager(discord.Client):
         self.blocked = False
         self.tree = app_commands.CommandTree(self)
 
-    async def on_ready(self) -> None:
+    async def setup_hook(self) -> None:
         await self.tree.sync(guild=self.env.guild)
 
     async def autoshutdown_wait(self) -> None:
