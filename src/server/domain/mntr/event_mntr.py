bb888ad9684136ef1f60e97cafc8999bec9ce79c
suuniqo <suuniqo@gmail.com>
2025-07-22 18:23:59 +0200

    fixed bug, didnt reset timeout after shutting the server due to inactivity

--- src/server/domain/mntr/event_mntr.py
+++ src/server/domain/mntr/event_mntr.py
@@ -89,6 +89,7 @@ class EventMntr(ServerMntr):
             self._ebus.emit(ServerEvent.EMPTY)
         elif time.time() - self._idle_since > self._idle_timeout:
             self._ebus.emit(ServerEvent.IDLE)
+            self._idle_since = None
 
     async def _monitor_loop(self) -> None:
         """
