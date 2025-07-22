be7d33becfb9ea02aa2a4563d839438ac7a08f7c
suuniqo <suuniqo@gmail.com>
2025-07-22 02:41:19 +0200

    reconfigured console logging

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import asyncio
+import logging
 
 from bot.factory import BotFactory
 from conf.loader.env_loader import EnvConfLoader
 
 
+def configure_logging() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s][%(asctime)s][%(name)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+
 async def main() -> None:
+    configure_logging()
+
     conf = EnvConfLoader.load()
     bot = await BotFactory.make(conf)
 
